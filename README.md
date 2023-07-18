92ab0d719e422ee1dbb5846880a1389c05aacb4e
Jairo(Jay) Velazquez <jairovelazquez101@gmail.com>
2023-07-17 22:10:04 -0700

    complete

---
+++ README.md
@@ -0,0 +1,41 @@
+# Quote Generator API
+API that serves up famous and interesting quotes about technology.
+
+
+## Table of Contents
+* [Technologies Used](#technologies-used)
+* [Features](#features)
+* [Setup](#setup)
+* [Usage](#usage)
+* [Project Status](#project-status)
+* [Contact](#contact)
+
+
+## Technologies Used
+- Express
+- Node.js
+- HTML 
+- CSS 
+- JavaScript 
+- JSON
+
+
+## Features
+- Fetch quotes by author, randomly or fetch all.
+- Add your own quotes.
+
+
+## Setup
+Clone this repo to your desktop and run npm install to install all the dependencies.
+
+
+## Usage
+Once the dependencies are installed, you can run npm start to start the application. You will then be able to access it at localhost:4001
+
+
+## Project Status
+Project is: _Complete_ 
+
+
+## Contact
+By Jairo(Jay) Velazquez
---
+++ data.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    quote: "Your time is limited, don't waste it living someone else's life.",
+    person: 'Steve Jobs'
+  },
+  {
+    quote: "I have not failed. I’ve just found 10,000 ways that won’t work.",
+    person: 'Thomas Edison'
+  },
+  {
+    quote: "Technology like art is a soaring exercise of the human imagination.",
+    person: 'Daniel Bell'
+  },
+  {
+    quote: "What new technology does is create new opportunities to do a job that customers want done.",
+    person: 'Tim O’Reilly'
+  },
+  {
+    quote: "In a world that's changing really quickly, the only strategy that is guaranteed to fail is not taking risks.",
+    person: 'Mark Zuckerberg'
+  },
+    {
+    quote: 'Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains.',
+    person: 'Bill Gates'
+  },
+  {
+    quote: 'The great growling engine of change – technology.',
+    person: 'Alvin Toffler'
+  },
+  {
+    quote: "When something is important enough, you do it even if the odds are not in your favor.",
+    person: 'Elon Musk'
+  },
+  {
+    quote: "We are stubborn on vision. We are flexible on details.",
+    person: 'Jeff Bezos'
+  },
+  {
+    quote: "The real problem is not whether machines think but whether men do.",
+    person: 'B.F. Skinner'
+  },
+  {
+    quote: 'Innovation is the outcome of a habit, not a random act.',
+    person: 'Sukant Ratnakar'
+  },
+  {
+    quote: 'Let’s go invent tomorrow instead of worrying about what happened yesterday.',
+    person: 'Steve Jobs'
+  }
+];
+
+module.exports = {
+  quotes
+};
